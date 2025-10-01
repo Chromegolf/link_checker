501fba66de3d0c7195fad0e9c4f52f6632926c49
ruslan <ruslanantipov@gmail.com>
2025-10-01 18:06:15 +0300

    refact

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 ##сгенерировать отчет из какой-нибудь папки
 ##переименовать отчет - для удобства работы. Можно переименовать в их код из ReportService
 ##DONE получить длину таблицы
-##из этой таблицы рандомно почекать ссылки из разных ячеек
+##DONE из этой таблицы рандомно почекать ссылки из разных ячеек
 ##DONE попытаться перейти по этим ссылкам
 import random
 
@@ -19,24 +19,30 @@ print(f'Max row in list: {max_row}')
 
 
 def get_issue_screen_link():
-    screen_list = []
+    screen_link_list = []
     for row in range(5, max_row):
         screen_link = ws[f'X{row + 1}'].value
-        screen_list.append(screen_link)
-    random_url = random.sample(screen_list, 3)
+        screen_link_list.append(screen_link)
+
+    random_url = random.sample(screen_link_list, 3)
 
     for row in range(len(random_url)):
         webbrowser.open(random_url[row]) ##проверить можно ли оптимизировать этот код?
 
 
 def get_issue_screen_check_link():
+    screen_check_list = []
     for row in range(5, max_row):
-        screen_link = ws[f'Y{row + 1}'].value
-        if screen_link is not None:
-            print(screen_link)
-            webbrowser.open(screen_link)
+        screen_check_link = ws[f'Y{row + 1}'].value
+        if screen_check_link is not None:
+            screen_check_list.append(screen_check_link)
         else:
             pass
+
+    random_url = random.sample(screen_check_list, 3)
+
+    for row in range(len(random_url)):
+        webbrowser.open(random_url[row]) ##проверить можно ли оптимизировать этот код?
 
 
 if __name__ == '__main__':
